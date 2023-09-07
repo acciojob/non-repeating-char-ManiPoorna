9f7936a6bd6016baa11580ca064454c220896c27
Mani Poorna <118202451+ManiPoorna@users.noreply.github.com>
2023-09-07 11:35:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let count = -1;
+  for (var i = 0; i < str.length; i++) {
+      console.log("iteration ",i+1)
+    for(let j=0;j<str.length;j++){
+      if(str.charAt(i) === str.charAt(j)){
+        count++;
+        console.log(count +" "+ i)
+      }
+    }
+    if(count == 0){
+      return str.charAt(i);
+    }
+    count = -1;
+  }
+  return null
 }
 const input = prompt("Enter a string");
+
 alert(firstNonRepeatedChar(input)); 
